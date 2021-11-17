9258529d70d008c23d451fbd15f56b7fb380c725
Manuel Alejandro Coronel Andrade <coronel.andrade.manuel@gmai.com>
2021-11-16 20:56:12 -0500

    Item con cantidad de disminucion ajustada

--- ecommerce_rest/apps/inventory/api/api.py
+++ ecommerce_rest/apps/inventory/api/api.py
@@ -99,9 +99,8 @@ def item_detail_view(request,pk=None):
     elif request.method == 'PUT':
         item = Item.objects.filter(id = pk).first()
         data = request.data
-        print(data['quantity'])
+        data['quantity'] = item.quantity - data['quantity']
         item_serializer = ItemSerializer(item,data = request.data)
-        print(item_serializer)
         if item_serializer.is_valid():
             item_serializer.save()
             return Response(item_serializer.data)

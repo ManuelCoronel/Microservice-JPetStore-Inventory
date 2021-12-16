a84eb25872aa11634c02265eb1c69f74aeadad25
Manuel Alejandro Coronel Andrade <coronel.andrade.manuel@gmai.com>
2021-12-16 00:11:06 -0500

    Manage modificado

--- ecommerce_rest/manage.py
+++ ecommerce_rest/manage.py
@@ -8,6 +8,10 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_rest.settings.local')
     try:
+        # Override default port for runserver command
+        from django.core.management.commands.runserver import Command as runserver
+        runserver.default_port = "7000"
+
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
@@ -16,7 +20,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
